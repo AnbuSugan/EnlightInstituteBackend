272e84b113f862e44c682359804d0c93957502bb
AnbuSugan <anbarasudhandapani18@gmail.com>
2023-12-05 17:40:44 +0530

    commit code

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 import express from "express";
 import mysql from "mysql";
 import cors from "cors";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import cookieParser from "cookie-parser";
+const salt = 10;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 const db = mysql.createConnection({
   host: "localhost",
@@ -23,21 +28,25 @@ db.connect((err) => {
 
 app.post("/createUser", (req, res) => {
   const sql = "INSERT INTO registers SET?";
+  bcrypt.hash(req.body.password.toString(), salt, (err, hash) => {
+    if (err) return res.json({ Error: "Error for hassing password" });
+    const values = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      date: req.body.date,
+      email: req.body.email,
+      mobileNumber: req.body.mobileNumber,
+      city: req.body.city,
+      state: req.body.state,
+      zip: req.body.zip,
+      userName: req.body.userName,
+      hash,
+    };
 
-  const values = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    date: req.body.date,
-    email: req.body.email,
-    mobileNumber: req.body.mobileNumber,
-    city: req.body.city,
-    state: req.body.state,
-    zip: req.body.zip,
-  };
-
-  db.query(sql, [values], (err, result) => {
-    if (err) throw err;
-    return res.json(result);
+    db.query(sql, [values], (err, result) => {
+      if (err) throw err;
+      return res.json(result);
+    });
   });
 });
 
